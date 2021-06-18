2e7a446cc59399519f92d3d023fc323739bf01ac
Atique Morshed Sami <iamsamix17@gmail.com>
2021-06-19 01:20:58 +0600

    +Client side Mutation +[FIN]

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -3,7 +3,7 @@ import MainHero from "../components/MainHero/MainHero"
 import CategoryDisplay from "../components/CategoryDisplay/CategoryDisplay"
 import CardDisplay from "../components/CardDisplay/CardDisplay"
 
-import {useQuery, gql} from '@apollo/client';
+import {useQuery, useMutation, gql} from '@apollo/client';
 
 const GET_ANIMALS = gql`
     query {
@@ -17,10 +17,36 @@ const GET_ANIMALS = gql`
     }
 `;
 
+const ADD_ANIMAL = gql`
+ mutation addAnimal(
+        $image: String!
+        $title: String!
+        $rating: Float!
+        $price: String!
+        $description: [String!]!
+        $stock: Int!
+        $slug: String!
+        $category: String!
+    ) {
+    addAnimal(
+        image: $image
+        title: $title
+        rating: $rating
+        price: $price
+        description: $description
+        stock: $stock
+        slug: $slug
+        category: $category
+    ) {
+        id
+    }
+ }
+`;
+
 function LandingPage() {
 
     const {loading, data, error} = useQuery(GET_ANIMALS);
-
+    const [addAnimal] = useMutation(ADD_ANIMAL);
     if(error) {
         return <h2>Error!</h2>;
     } 
@@ -33,6 +59,18 @@ function LandingPage() {
                 <MainHero />
                 <CategoryDisplay />
                 <CardDisplay animals={data.animals}/>
+                <button onClick={() => addAnimal({
+                    variables: {
+                        image: "cat",
+                        title: "Big BOi Cat",
+                        rating: 5.0,
+                        price: "69,123",
+                        description: ["YOYO", "YELLO"],
+                        stock: 18,
+                        slug: "catto",
+                        category: "1"
+                    }
+                })}>ADD ANIMAL</button>
             </div>
         );
     }
